3dcfffe6514119469f5d41c9c1f1db92639675aa
Jessie Hsieh <jessiehsieh86@gmail.com>
2022-08-29 16:12:15 +0800

    Add read file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+#讀取檔案
 products = [] #大清單
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品, 價格' in line:
+			continue #跳到下一次迴圈_跳過商品價個那一行，其他行繼續
+		name, price = line.strip().split(',') #strip()去掉\n_split()檔案中遇到逗點就split
+		products.append([name, price])
+print(products)
+
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q': #quit
@@ -9,6 +19,7 @@ while True:
 	p.append(price) #8,9行等同於 p = [name, product]
 	products.append(p) #大清單裝小清單_二維清單
 
+#印出所有購買紀錄
 for p in products:
 	print(p[0], '的價格是', p[1]) #每一個小清單的第０格都是name,第一格是price
 
